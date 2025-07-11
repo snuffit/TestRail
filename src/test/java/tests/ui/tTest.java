83272efc5f9d256a890acf94059e5b470fa8d1e7
bludich <sadpufikot@gmail.com>
2025-07-11 12:54:01 +0300

    add tTest

---
+++ src/test/java/tests/ui/tTest.java
@@ -0,0 +1,13 @@
+package tests.ui;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class tTest {
+
+    @Test
+    public void test() {
+        int a = 2;
+        Assert.assertEquals(a, 2);
+    }
+}
